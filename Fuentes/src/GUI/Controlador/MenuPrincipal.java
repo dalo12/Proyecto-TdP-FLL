c8392e5ee89aa8f3c7bfc511212764b5763ae7c6
Latouquette96 <davidemanuel96@outlook.com>
2019-09-10 17:35:43 -0300

    Creación de botón tienda

--- Fuentes/src/GUI/Controlador/MenuPrincipal.java
+++ Fuentes/src/GUI/Controlador/MenuPrincipal.java
@@ -1,20 +1,25 @@
 package GUI.Controlador;
 
 import java.awt.EventQueue;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-import javax.swing.border.MatteBorder;
-
-import GUI.Mapa.PanelMapa;
 
 import java.awt.Color;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
+import javax.swing.border.EtchedBorder;
+
+import GUI.Tienda.BotonTienda;
+
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class MenuPrincipal {
+	//Atributos de GUI
 	private JFrame frame;
+	private JPanel panelJuego;
 
 	/**
 	 * Launch the application.
@@ -44,18 +49,59 @@ public class MenuPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 764, 423);
+		frame.setBounds(100, 100, 900, 420);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new MigLayout("", "[738px,grow,fill]", "[60px][305px,grow,fill]"));
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 738, 60);
-		frame.getContentPane().add(panel);
+		panel.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLACK, Color.DARK_GRAY));
+		frame.getContentPane().add(panel, "cell 0 0,grow");
+		panel.setLayout(new MigLayout("", "[90px:90px:90px,fill][90px:90px:90px,fill][90px:90px:90px,fill][90px:90px:90px,fill][90px:90px:90px,fill][][][][][2px,center][grow,fill][grow,fill]", "[60px:60.00px:60px,fill]"));
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(10, 78, 738, 305);
-		frame.getContentPane().add(panel_1);
+		BotonTienda btnCharacter1 = new BotonTienda();
+		panel.add(btnCharacter1, "cell 0 0,grow");
+		
+		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton, "flowx,cell 5 0");
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panel.add(btnNewButton_2, "cell 6 0");
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel.add(btnNewButton_1, "cell 8 0");
+		
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setBackground(Color.BLACK);
+		panel.add(separator, "cell 9 0,alignx left,growy");
+		
+		JButton btnCharacter2 = new JButton("");
+		panel.add(btnCharacter2, "cell 1 0,grow");
+		
+		JButton btnCharacter3 = new JButton("");
+		panel.add(btnCharacter3, "cell 2 0,grow");
+		
+		JButton btnCharacter4 = new JButton("");
+		panel.add(btnCharacter4, "cell 3 0,grow");
+		
+		JButton btnCharacter5 = new JButton("");
+		panel.add(btnCharacter5, "cell 4 0,grow");
+		
+		JLabel lblPuntaje = new JLabel("- PTS.");
+		lblPuntaje.setFont(new Font("Tahoma", Font.BOLD, 17));
+		panel.add(lblPuntaje, "cell 11 0,grow");
+		
+		JLabel lblMoneda = new JLabel("- $");
+		lblMoneda.setFont(new Font("Tahoma", Font.BOLD, 17));
+		panel.add(lblMoneda, "cell 10 0,grow");
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		panel.add(btnNewButton_3, "cell 7 0");
+		
+		panelJuego = new JPanel();
+		panelJuego.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLACK, Color.DARK_GRAY));
+		frame.getContentPane().add(panelJuego, "cell 0 1,grow");
+		panelJuego.setLayout(null);
 	}
 }
