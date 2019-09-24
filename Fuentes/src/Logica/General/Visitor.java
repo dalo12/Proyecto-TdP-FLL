9c2e6442e611bd8c0d46fe61828226353a052e9c
AgusF22 <49928014+AgusF22@users.noreply.github.com>
2019-09-24 18:26:01 -0300

    Agregada clase visitor

---
+++ Fuentes/src/Logica/General/Visitor.java
@@ -0,0 +1,11 @@
+package Logica.General;
+
+public abstract class Visitor {
+
+	public abstract void visitEnemigo();
+	
+	public abstract void visitPersonaje();
+	
+	public abstract void visitObjeto();
+	
+}
---
+++ Fuentes/src/Logica/General/VisitorAccion.java
@@ -0,0 +1,23 @@
+package Logica.General;
+
+public class VisitorAccion extends Visitor {
+
+	@Override
+	public void visitEnemigo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitPersonaje() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitObjeto() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
