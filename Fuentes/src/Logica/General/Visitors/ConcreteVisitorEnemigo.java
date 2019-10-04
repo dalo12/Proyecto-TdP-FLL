042558087627489234b32ffcc56caea73d061f5d
AgusF22 <49928014+AgusF22@users.noreply.github.com>
2019-10-04 00:12:20 -0300

    Añadidas las clases concretas de visitor

---
+++ Fuentes/src/Logica/General/Visitors/ConcreteVisitorEnemigo.java
@@ -0,0 +1,41 @@
+package Logica.General.Visitors;
+
+import Logica.General.Aliado;
+import Logica.General.Disparo;
+import Logica.General.Enemigo;
+import Logica.General.Objeto;
+import Logica.General.Premio;
+
+public class ConcreteVisitorEnemigo extends Visitor {
+
+	@Override
+	public void visitEnemigo(Enemigo enemigo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitAliado(Aliado aliado) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitObjeto(Objeto objeto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitObjeto(Disparo disparo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitObjeto(Premio premio) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
