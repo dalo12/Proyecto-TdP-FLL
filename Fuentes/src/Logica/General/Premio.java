df554a7bc82bf801bbbdf1cc8191c0855f0534dc
AgusF22 <49928014+AgusF22@users.noreply.github.com>
2019-10-05 02:15:01 -0300

    Actualizado Premio.java

--- Fuentes/src/Logica/General/Premio.java
+++ Fuentes/src/Logica/General/Premio.java
@@ -1,5 +1,7 @@
 package Logica.General;
 
+import Logica.General.Visitors.Visitor;
+
 /**
  * Modela los powerups
  * @author Comisión 25 (FERRANTE, LATOUQUETTE, LÓPEZ)
@@ -13,6 +15,20 @@ public abstract class Premio extends GameObject {
 	
 	public void activar() {
 		//TODO debe recorrer todos los objetos del juego enviando su visitor.
+	}
+
+	@Override
+	public void accionar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Acepta un visitor.
+	 */
+	@Override
+	public void accept(Visitor v) {
+		v.visitPremio(this);
 	}
 	
 }
